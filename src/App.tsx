7d1d94db6c841f3e79bd50a0547b48e518e8f79c
Igor Santos <igorsantos9513@outlook.com>
2024-02-23 18:10:21 -0300

    feat: added Main form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Routes } from "./routes";
 
 function App() {
   return (
-    <>
+    <div className="bg-slate-900 h-screen w-screen flex justify-center">
       <Routes />
-    </>
+    </div>
   );
 }
 
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,38 @@
+import { Github, Plus } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
 export function Main() {
+  const [newRepo, setNewRepo] = useState("");
+
+  function handleInputValue(e: ChangeEvent<HTMLInputElement>) {
+    setNewRepo(e.target.value);
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
   return (
-    <div>
-      <h1>hello world!</h1>
+    <div className="antialiased p-4 mt-12 items-start w-[700px] h-[150px] rounded-md flex flex-col bg-white">
+      <div className="p-4 flex gap-2 w-full items-start">
+        <Github size={25} />
+        <h1 className="font-bold">Meus Repositórios</h1>
+      </div>
+
+      <form
+        action=""
+        className="p-4 flex items-center justify-center w-full gap-2"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Adicionar Repositorio"
+          className="py-2 px-3 flex-1 focus-visible:none focus:none outline-none"
+          value={newRepo}
+          onChange={handleInputValue}
+        />
+        <button type="submit">
+          <Plus />
+        </button>
+      </form>
     </div>
   );
 }
