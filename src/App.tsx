b99bf3f8672c1f178ec0081353177774db0e38aa
Igor Santos <igorsantos9513@outlook.com>
2024-02-26 19:27:15 -0300

    feat: repo listing and improved UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes } from "./routes";
 
 function App() {
   return (
-    <div className="bg-slate-900 h-screen w-screen flex justify-center">
+    <div className="bg-slate-900 h-screen w-screen flex justify-center items-start">
       <Routes />
     </div>
   );
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import { Github, Plus } from "lucide-react";
+import { Github, Plus, Loader, Trash2, Menu, Trash } from "lucide-react";
 import { ChangeEvent, FormEvent, useCallback, useState } from "react";
 import { api } from "../../services/api";
 
@@ -8,6 +8,7 @@ export function Main() {
   }
   const [newRepo, setNewRepo] = useState("");
   const [repository, setRepository] = useState<object[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleInputValue(e: ChangeEvent<HTMLInputElement>) {
     setNewRepo(e.target.value);
@@ -18,13 +19,20 @@ export function Main() {
       e.preventDefault();
 
       async function submit() {
-        const response = await api.get(`repos/${newRepo}`);
-        const data: Data = {
-          name: response.data.full_name,
-        };
+        setIsLoading(true);
+        try {
+          const response = await api.get(`repos/${newRepo}`);
+          const data: Data = {
+            name: response.data.full_name,
+          };
 
-        setRepository([...repository, data]);
-        setNewRepo("");
+          setRepository([...repository, data]);
+          setNewRepo("");
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setIsLoading(false);
+        }
       }
 
       submit();
@@ -32,7 +40,7 @@ export function Main() {
     [newRepo, repository]
   );
   return (
-    <div className="antialiased p-4 mt-12 items-start w-[700px] h-[150px] rounded-md flex flex-col bg-white">
+    <div className="antialiased p-4 mt-12 items-start w-[700px] min-h-[150px] rounded-md flex flex-col bg-white">
       <div className="p-4 flex gap-2 w-full items-start">
         <Github size={25} />
         <h1 className="font-bold">Meus Repositórios</h1>
@@ -46,14 +54,30 @@ export function Main() {
         <input
           type="text"
           placeholder="Adicionar Repositorio"
-          className="py-2 px-3 flex-1 focus-visible:none focus:none outline-none"
+          className="py-2 px-3 flex-1 focus-visible:none focus:none outline-none bg-slate-100 rounded-sm"
           value={newRepo}
           onChange={handleInputValue}
         />
-        <button type="submit">
-          <Plus />
+        <button disabled={isLoading} type="submit">
+          {isLoading ? (
+            <Loader className="animate-spin" />
+          ) : (
+            <Plus className="" />
+          )}
         </button>
       </form>
+
+      <ul className="p-4 flex flex-col justify-between w-full">
+        {repository?.map((repo) => (
+          <div className="flex w-full justify-between pt-6 border-2 p-2 border-t-0 border-l-0 border-r-0 border-b-slate-600">
+            <div className="flex flex-row jus items-center gap-2">
+              <Trash2 className="cursor-pointer" />
+              <li key={repo.name}>{repo.name}</li>
+            </div>
+            <Menu className="cursor-pointer" />
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
